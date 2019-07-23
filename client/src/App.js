37409063fbdb2bf779c4f63c07e7020c0dfd6616
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-23 18:45:31 -0400

    installed react and created client folder

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.png';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="SpaceX" style={{width: 300, display: 'block', margin: 'auto'}} />
+    </div>
+  );
+}
+
+export default App;
