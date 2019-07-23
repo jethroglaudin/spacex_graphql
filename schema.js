bd22c163a3c690616bafaf4d650605cc3ca524f2
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-22 22:36:07 -0400

    created Root Query that makes an get request to the spacex api

--- schema.js
+++ schema.js
@@ -1,13 +1,17 @@
+const axios = require('axios');
+
 const {
   GraphQLObjectType,
   GraphQLInt,
   GraphQLString,
-  GraphQLBoolean
-} = require("graphql");
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema
+} = require('graphql');
 
-//Launch Type
+// Launch Type
 const LaunchType = new GraphQLObjectType({
-  name: "Launch",
+  name: 'Launch',
   fields: () => ({
     flight_number: { type: GraphQLInt },
     mission_name: { type: GraphQLString },
@@ -19,12 +23,32 @@ const LaunchType = new GraphQLObjectType({
 });
 
 // Rocket Type
-
 const RocketType = new GraphQLObjectType({
-  name: "Rocket",
+  name: 'Rocket',
   fields: () => ({
     rocket_id: { type: GraphQLString },
     rocket_name: { type: GraphQLString },
     rocket_type: { type: GraphQLString }
   })
+});
+
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      resolve(parent, args) {
+        return axios
+          .get('https://api.spacexdata.com/v3/launches')
+          .then(res => res.data);
+      }
+    }
+  }
+    
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
