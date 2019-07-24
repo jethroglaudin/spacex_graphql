7f1fdc0b55b02ca40633fceb6b555be0035114e1
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-23 23:40:26 -0400

    added apollo client and apollo provider. Creted a new ApolloClient and added uri to localhost for graphql from our backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import logo from './logo.png';
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import Launches from '../src/components/Launches';
 
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <img src={logo} alt="SpaceX" style={{width: 300, display: 'block', margin: 'auto'}} />
-    </div>
-  );
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="container">
+          <img
+            src={logo}
+            alt="SpaceX"
+            style={{ width: 300, display: "block", margin: "auto" }}
+          />
+          <Launches />
+        </div>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
