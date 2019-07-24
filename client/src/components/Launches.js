8ca810fc4725829ad552ff37e6a6bc29e5c878fa
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-23 23:38:06 -0400

    created Launches.js

---
+++ client/src/components/Launches.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+const Launches_Query = gql`
+ query LaunchesQuery {
+     launches {
+        flight_number
+        mission_name
+        launch_date_local
+        launch_success
+    }
+ }
+
+`;
+
+export class Launches extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="display-4 my-3">Launches</h1>
+      </div>
+    );
+  }
+}
+
+export default Launches;
